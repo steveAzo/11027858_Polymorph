3ec3230b628993a8e5de2d00543cd8608d4e7210
steveAzo -Git <stephenazongo77@gmail.com>
2023-12-18 09:09:34 +0000

    a minor change

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 require('dotenv').config()
+const PORT = process.env.PORT || 3000;
 
 import mongoose from "mongoose";
 import { connectDb } from './helpers/database.js'
 
+
 await connectDb("mongoose:127.0.0")
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
